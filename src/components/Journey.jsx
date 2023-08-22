b072de389528a164ed0f586fb3759855e2f1b673
AslamThachapalli <aslam.develop912@gmail.com>
2023-08-22 22:03:17 +0530

    my journey templates made

---
+++ src/components/Journey.jsx
@@ -0,0 +1,23 @@
+const Journey = ({ period, company, title, responsibilities }) => {
+  return (
+    <div className="row mt-4">
+      <div className="col-md-5">
+        <h5>{period}</h5>
+        <h6>{company}</h6>
+      </div>
+
+      <div className="col-md-7">
+        <h5 className="mt-4 mt-md-0">{title}</h5>
+        <ul class="list-group list-group-flush">
+          {responsibilities.map((responsibility) => (
+            <li class="list-group-item list-group-item-action">
+              {responsibility}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Journey;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Journey from "../components/Journey";
 
 const About = () => {
   return (
@@ -7,19 +8,68 @@ const About = () => {
       <Header currentTab="About" />
       <div className="container">
         <div className="row d-flex justify-content-center">
-          <h1 className="mt-4">Aslam Thachapalli</h1>
-          <h2 className="mt-2">
-            Mexican artist, I mainly work with oils, color pencils and gouache.
-            Passionate about celebrating diversity through my work, weaving
-            stories through shapes and colours, inspired by feelings, dreams and
-            everyday life. My art intends to represent strong confident
-            characters living in vibrant worlds which are often a fusion between
-            real life and my imagination. I play with the disruption of the
-            human form to reveal my view on both internal and external beauty.
-          </h2>
-          <h1 className="mt-4">My Journey</h1>
+          <div className="col-11 col-lg-8">
+            <h1 className="mt-4">Aslam Thachapalli</h1>
+            <h2 className="mt-2">
+              Mexican artist, I mainly work with oils, color pencils and
+              gouache. Passionate about celebrating diversity through my work,
+              weaving stories through shapes and colours, inspired by feelings,
+              dreams and everyday life. My art intends to represent strong
+              confident characters living in vibrant worlds which are often a
+              fusion between real life and my imagination. I play with the
+              disruption of the human form to reveal my view on both internal
+              and external beauty.
+            </h2>
+            <h1 className="mt-4">My Journey</h1>
+            <div className="mt-2">
+              <Journey
+                period="July 2023 - Present"
+                company="Digichorus Technologies Pvt Ltd"
+                title="Flutter Developer"
+                responsibilities={[
+                  "This is a Task That i did on the company.",
+                  "This is a Task That i did on the company. This task a bit long",
+                  "This is a small Responsibility",
+                ]}
+              />
+
+              <Journey
+                period="Nov 2022 - May 2023"
+                company="Kohbee Technologies Pvt Ltd"
+                title="Flutter Developer Intern"
+                responsibilities={[
+                  "This is a Task That i did on the company.",
+                  "This is a Task That i did on the company. This task a bit long",
+                  "This is a small Responsibility",
+                ]}
+              />
+
+              <Journey
+                period="Jan 2022 - May 2023"
+                company="Brain Podium Technologies Pvt Ltd"
+                title="Freelance Flutter Developer"
+                responsibilities={[
+                  "This is a Task That i did on the company.",
+                  "This is a Task That i did on the company. This task a bit long",
+                  "This is a small Responsibility",
+                ]}
+              />
+
+              <Journey
+                period="July 2022"
+                company="College of Engineering Trivandrum"
+                title="Graduation"
+                responsibilities={[
+                  "This is a Task That i did on the company.",
+                  "This is a Task That i did on the company. This task a bit long",
+                  "This is a small Responsibility",
+                ]}
+              />
+            </div>
+          </div>
         </div>
       </div>
+      <br /> <br />
       <Footer></Footer>
     </>
   );
