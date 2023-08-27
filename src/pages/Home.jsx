538f50385fb9cefda71126bb2d8e277a0da0b56c
AslamThachapalli <aslam.develop912@gmail.com>
2023-08-27 13:01:57 +0530

    added head icon and updated fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
               >
                 ABOUT ME
               </span>{" "}
-              here ;)
+              here :)
             </h2>
           </div>
         </div>
